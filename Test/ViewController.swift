5ec4ca3ede86ce906c28001963e099b65b90f2a7
mentor-takechi <jp.techacademy@mentor-takechi>
2019-01-26 12:15:56 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by take on 2019/01/26.
+//  Copyright © 2019 mentor-takechi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
